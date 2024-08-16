448360149d8316da3ec7c63729648373aaef0bd3
syf <2282763709@qq.com>
2024-08-16 14:13:22 +0800

    2024-8-16 14.13

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   // base: process.env.NODE_ENV === 'production'?"/leetcodevue/":"/",
-  // base:'/leetcodevue/',
-  // build:{
-  //   outDir:"docs"
-  // },
+  base:'/leetcodevue/',
+  build:{
+    outDir:"docs"
+  },
   plugins: [
     vue(),
   ],
